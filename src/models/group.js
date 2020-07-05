d364391feedf23657046a082fb1ae63a80826d4e
Andrei Dolhaniuk <andrwandrewdd@gmail.com>
2020-07-05 13:48:40 +0300

    Add static method

--- src/models/group.js
+++ src/models/group.js
@@ -9,4 +9,18 @@ const groupSchema = new mongoose.Schema({
   students: [{ type: mongoose.Schema.ObjectId, ref: 'Student' }],
 });
 
+// eslint-disable-next-line func-names
+groupSchema.statics.findOneOrCreate = async function (number) {
+  const Group = this;
+  try {
+    const group = await Group.findOne({ number });
+    if (group) {
+      return group;
+    }
+    return await new Group({ number }).save();
+  } catch {
+    return null;
+  }
+};
+
 module.exports = mongoose.model('Group', groupSchema);
