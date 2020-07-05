0aee725f132379196eec72ad16df2fcb001bad4d
Andrei Dolhaniuk <andrwandrewdd@gmail.com>
2020-07-05 20:54:34 +0300

    Add controller to handle reading of all lessons and test for it

--- src/controllers/readLessons.js
+++ src/controllers/readLessons.js
@@ -1,5 +1,12 @@
-const readLessons = (req, res) => {
-  res.send({ lessons: [] });
+const Lesson = require('../models/lesson');
+
+const readLessons = async (req, res) => {
+  try {
+    const lessons = await Lesson.find().populate('group').populate('teacher');
+    res.send({ lessons });
+  } catch (e) {
+    res.send({ error: e.message });
+  }
 };
 
 module.exports = readLessons;
--- test/readLessons.test.js
+++ test/readLessons.test.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-underscore-dangle */
 const request = require('supertest');
+const mongoose = require('mongoose');
 const app = require('../src/app');
+const Lesson = require('../src/models/lesson');
+const Group = require('../src/models/group');
+const Teacher = require('../src/models/teacher');
+const { LESSSONS_DATA, TEACHER_DATA, GROUP_DATA } = require('./testData');
 
 const URL = '/lessons';
+
+// eslint-disable-next-line no-undef
+beforeEach(async () => {
+  await Lesson.deleteMany();
+  await Group.deleteMany();
+  await Teacher.deleteMany();
+});
 
 // eslint-disable-next-line no-undef
 describe('Test read operation', () => {
@@ -11,10 +24,44 @@ describe('Test read operation', () => {
     // eslint-disable-next-line no-undef
     expect(response.statusCode).toBe(200);
   });
+
   // eslint-disable-next-line no-undef
-  test('Response body should not be null.', async () => {
+  test('Read the lessons. Response body should not be null.', async () => {
     const response = await request(app).get(URL);
     // eslint-disable-next-line no-undef
     expect(response.body).not.toBeNull();
   });
+
+  // eslint-disable-next-line no-undef
+  test('Read the lessons. Request body should be an array with provided objects', async () => {
+    const groupDocument = await Group.create(GROUP_DATA);
+    const teacherDocument = await Teacher.create(TEACHER_DATA);
+
+    LESSSONS_DATA[0].teacher = teacherDocument._id;
+    LESSSONS_DATA[1].teacher = teacherDocument._id;
+    LESSSONS_DATA[0].group = groupDocument._id;
+    LESSSONS_DATA[1].group = groupDocument._id;
+    await Lesson.insertMany(LESSSONS_DATA);
+    const lessons = await Lesson.find().populate('teacher').populate('group');
+    const response = await request(app).get(URL);
+    // eslint-disable-next-line no-undef
+    expect(response.body).toMatchObject(
+      {
+        lessons: lessons.map((each) => each.toObject(
+          {
+            transform: (doc, ret) => {
+              // eslint-disable-next-line no-param-reassign
+              ret._id = ret._id.toString();
+              return ret;
+            },
+          },
+        )),
+      },
+    );
+  });
+});
+
+// eslint-disable-next-line no-undef
+afterAll(() => {
+  mongoose.connection.close();
 });
